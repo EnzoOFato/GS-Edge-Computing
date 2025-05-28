1a60bd00b3d64ae76e6298fc8ef21fa831852d0e
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-05-28 20:46:27 -0300

    Feat: instalação DHT22

--- code.cpp
+++ code.cpp
@@ -1,9 +1,23 @@
-void setup {
-    pinMode(3, OUTPUT);
+#include <DHT.h>;
+
+#define dhtPin 7
+#define dhtTipo DHT22
+DHT dht(dhtPin, dhtTipo);
+
+float umidade;
+float temp;
+
+void setup () {
+    Serial.begin(115200);
+    dht.begin();
 }
-void loop {
-    digitalWrite(3, HIGH);
-    delay(1000);    
-    digitalWrite(3, LOW);
-    delay(1000);
-}j
+void loop () {
+  delay(2000);
+  umidade = dht.readHumidity();
+  temp = dht.readTemperature();
+  Serial.println("Temperatura: "+ (String)temp+"C°");
+  delay(2000);
+  Serial.println("Umidade do ar: "+ (String)umidade+"%");
+  delay(2000);
+}
+
