2c25520804916e0ba5bf24288d314f781d0699ee
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-05-29 00:08:50 -0300

    Feat: inclusão LCD

--- nivel.cpp
+++ nivel.cpp
@@ -1,3 +1,9 @@
+#include <LiquidCrystal_I2C.h>;
+#define LCDADDR 0x27
+#define LCDColunas 16
+#define LCDFileiras 2
+LiquidCrystal_I2C lcd(LCDADDR, LCDColunas, LCDFileiras);
+
 #define buzzPin 9
 #define trigPin 3
 #define echoPin 2
@@ -13,6 +19,9 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
   Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
@@ -26,26 +35,48 @@ void loop() {
   Serial.println(distancia);
 
   if(distancia > 200){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Baixo Risco");
+    lcd.setCursor(0,1);
+    lcd.print("de Desastre");
     digitalWrite(7, HIGH);
-    delay(1000);
+    delay(4000);
     digitalWrite(7, LOW);
   }
   else if(distancia <= 200 && distancia > 80){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Medio Risco");
+    lcd.setCursor(0,1);
+    lcd.print("de Desastre");
     digitalWrite(6, HIGH);
-    delay(1000);
+    delay(4000);
     digitalWrite(6, LOW);
   }
   else if(distancia <= 80 && distancia >15){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Alto Risco");
+    lcd.setCursor(0,1);
+    lcd.print("de Desastre");
     digitalWrite(5, HIGH);
-    delay(1000);
+    delay(4000);
     digitalWrite(5, LOW);
   }
   else{
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Risco iminente");
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Autoridades");
+    lcd.setCursor(0,1);
+    lcd.print("Contatadas");
     digitalWrite(5, HIGH);
     digitalWrite(buzzPin, HIGH);
-    delay(1000);
+    delay(2000);
     digitalWrite(buzzPin, LOW);
   }
-
-  delay(3000);
 }
