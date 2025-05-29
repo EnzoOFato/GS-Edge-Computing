fbaedaa5bd58c17770a8a5256bde32f3d6659cd8
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-05-28 23:52:05 -0300

    Feat: config_geral

--- nivel.cpp
+++ nivel.cpp
@@ -16,7 +16,7 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(buzzPin, HIGH);
+  digitalWrite(5, LOW);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(20);
   digitalWrite(trigPin, LOW);
@@ -24,5 +24,28 @@ void loop() {
   tempo = pulseIn(echoPin, HIGH);
   distancia = tempo/58;
   Serial.println(distancia);
+
+  if(distancia > 200){
+    digitalWrite(7, HIGH);
+    delay(1000);
+    digitalWrite(7, LOW);
+  }
+  else if(distancia <= 200 && distancia > 80){
+    digitalWrite(6, HIGH);
+    delay(1000);
+    digitalWrite(6, LOW);
+  }
+  else if(distancia <= 80 && distancia >15){
+    digitalWrite(5, HIGH);
+    delay(1000);
+    digitalWrite(5, LOW);
+  }
+  else{
+    digitalWrite(5, HIGH);
+    digitalWrite(buzzPin, HIGH);
+    delay(1000);
+    digitalWrite(buzzPin, LOW);
+  }
+
   delay(3000);
 }
