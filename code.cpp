d1b5869e8dbad4038183a146774a57d364ca757e
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-05-28 22:30:12 -0300

    Feat: adição LCD

--- code.cpp
+++ code.cpp
@@ -1,5 +1,10 @@
-#include <DHT.h>;
+#include <LiquidCrystal_I2C.h>;
+#define LCDADDR 0x27
+#define LCDColunas 16
+#define LCDFileiras 2
+LiquidCrystal_I2C lcd(LCDADDR, LCDColunas, LCDFileiras);
 
+#include <DHT.h>;
 #define dhtPin 7
 #define dhtTipo DHT22
 DHT dht(dhtPin, dhtTipo);
@@ -10,13 +15,55 @@ float temp;
 void setup () {
     Serial.begin(115200);
     dht.begin();
+    lcd.begin(16,2);
+    lcd.init();
+    lcd.backlight();
 }
 void loop () {
-  delay(2000);
   umidade = dht.readHumidity();
   temp = dht.readTemperature();
+
+  if(umidade > 80){
+    lcd.clear();
+    delay(500);
+    lcd.setCursor(5,0);
+    lcd.print("Alerta");
+    lcd.setCursor(1,1);
+    lcd.print("Chuva eminente");
+    delay(3000);
+    lcd.clear();
+    delay(500);
+  }
+  else if (umidade > 50 && umidade <= 79){
+    lcd.clear();
+    delay(500);
+    lcd.setCursor(5,0);
+    lcd.print("Médio");
+    lcd.setCursor(1,1);
+    lcd.print("Risco de chuva");
+    delay(3000);
+    lcd.clear();
+    delay(500);
+  }
+  else {
+    lcd.clear();
+    delay(500);
+    lcd.setCursor(5,0);
+    lcd.print("Baixo");
+    lcd.setCursor(1,1);
+    lcd.print("Risco de chuva");
+    delay(3000);
+    lcd.clear();
+    delay(500);
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: "+(String)temp+"C");
+  lcd.setCursor(0, 1);
+  lcd.print("Umidade: "+(String)umidade+"%");
+
   Serial.println("Temperatura: "+ (String)temp+"C°");
-  delay(2000);
+  delay(1000);
   Serial.println("Umidade do ar: "+ (String)umidade+"%");
   delay(2000);
 }
